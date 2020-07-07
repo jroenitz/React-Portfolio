3db7b8ce9778eed1dd4c929997a1b2f9f97e4088
Jordan Roenitz <61602629+jroenitz@users.noreply.github.com>
2020-07-07 17:56:52 -0500

    nav bar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,23 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Button, Nav, Navbar, Form, FormControl } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Navbar bg="primary" variant="dark">
+    <Navbar.Brand href="#home">Home</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#Bio">Bio</Nav.Link>
+      <Nav.Link href="#features">Projects</Nav.Link>
+      <Nav.Link href="#pricing">Resume</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-light">Search</Button>
+    </Form>
+  </Navbar>
+    
+  
+      <h1>Welcome to Jordans Porfolio</h1>
+   <Button variant="primary">Primary</Button>{' '}
     </div>
   );
 }
